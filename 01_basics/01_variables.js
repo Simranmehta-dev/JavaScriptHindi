5c2015fc052a15d50a1d97d9ca0941204a9edf78
Simranmehta-dev <116286250+Simranmehta-dev@users.noreply.github.com>
2023-11-24 15:36:42 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144553;
+let accountEmail= "sm48@gmail.com"
+var accountPassword="1234"
+accountCity="Jaipur"
+
+//accountId=2;
+
+accountEmail="abs";
+accountPassword="jhbfjh"
+accountCity="abc"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+
+//alert("hell0");//we are using node js not browser
+
+//code readability shppould be high
+
+let name="simran"
+let age =18
+let isLogin=false
+
+
+//number=> 2 to the power 52
+//bigint
+//string==>""
+//boolean==>true / false
+//null==>stand alone value(khali value)
+//undefined==>
+//symbol ==>unique
+
+//object
+
+console.log(typeof "simran");
