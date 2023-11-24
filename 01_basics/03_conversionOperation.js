c6f931c73140c8053092c1807163e4554d95fba3
Simranmehta-dev <116286250+Simranmehta-dev@users.noreply.github.com>
2023-11-24 15:48:52 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+// let score="33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+
+//"33"=>33
+//"33abc"=>Nan
+//true=>1
+//false=>0
+
+let login=true
+let boolVal=Boolean(login)
+
